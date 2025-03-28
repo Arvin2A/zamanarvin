82985f6cdbbca61c05db3563bd5c9eed181dcb58
Arvin2A <mohsina111zaman@gmail.com>
2025-03-28 17:46:39 -0400

    BIG UPDT

---
+++ script.js
@@ -0,0 +1,17 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+    if (evt) {
+        evt.currentTarget.className += " active";
+    }
+}
